421ed45a8ee52904c19777bf3068264d9dd59d66
Adrian Ayarde <aayarde@overactive.com>
2019-11-28 13:37:11 -0300

    Structure, package, classes, etc.

---
+++ src/main/java/com/item/microservice/ItemMicorserviceApplication.java
@@ -0,0 +1,13 @@
+package com.item.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemMicorserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemMicorserviceApplication.class, args);
+    }
+
+}
