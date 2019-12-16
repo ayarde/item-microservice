96c4bf1329069de394df7569bb6e3b545b9ab5c9
Adrián Ayarde <adrian.ayarde@gmail.com>
2019-12-15 23:33:48 -0300

    Configuration to different environment.

--- src/main/java/com/item/microservice/controller/ItemController.java
+++ src/main/java/com/item/microservice/controller/ItemController.java
@@ -4,9 +4,13 @@ import com.item.microservice.model.Item;
 import com.item.microservice.model.Product;
 import com.item.microservice.model.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +21,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RefreshScope
 @RestController
 public class ItemController {
+
+    private static Logger log = LoggerFactory.getLogger(ItemController.class);
+    @Autowired
+    private Environment environment;
 
     @Autowired
     //@Qualifier("serviceRestTemplate")
@@ -53,9 +62,15 @@ public class ItemController {
 
     @GetMapping("/get-config")
     public ResponseEntity<?> getConfiguration(@Value("${server.port}") String port) {
+        log.info(text);
+
         Map<String, String> json = new HashMap<>();
         json.put("text", text);
         json.put("port", port);
+        if(environment.getActiveProfiles().length > 0 && environment.getActiveProfiles()[0].equals("dev")){
+            json.put("author.name", environment.getProperty("configuration.author.name"));
+            json.put("author.email", environment.getProperty("configuration.author.email"));
+        }
         return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 
